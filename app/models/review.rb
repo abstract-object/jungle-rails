cf4ac09c4ff26a5d199cf16da5d2671a6fbb2662
Jonathan Liu <jxliu95@gmail.com>
2019-07-11 14:19:44 +0000

    Add validations for review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,9 @@
 class Review < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :product
+
+  validates :product_id, presence: true, numericality: true
+  validates :user_id, presence: true, numericality: true
+  validates :description, presence: true
+  validates :rating, presence: true, numericality: { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 5 }
 end
